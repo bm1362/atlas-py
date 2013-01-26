8c9737f099409dae2a7864623403b8ded2950cc3
Ben Mays <bm1362@txstate.edu>
2013-01-26 17:22:43 -0600

    Added basic structure- subject to change.

---
+++ atlas/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+	
+	"simulation",	# the entry point for our simulation, contains control logic
+	"entity",		# represents any object involved in the simulation
+	"phys", 		# represents the 'physical' world, responsible for running physics logic
+	"scene",		# represents what is drawn to the screen
+	"map"			# contains objects that are placed absolutely- a scene renders only what is 'visible' in the map.
+]
