4e55917fbc84e3024e87cf7e18983c02560283a8
Kyle Jones <kyle@jonesetc.com>
2013-01-26 18:33:59 -0600

    added a few more stubs to the entity super class

--- atlas/entity.py
+++ atlas/entity.py
@@ -1,3 +1,13 @@
 class Entity(object):
 	def __init__(self, **kwargs):
-		self.position = dict(x = 0, y = 0)
+		self.position = dict(x=0, y=0)
+        self.linearVelocity = dict(x=0, y=0)
+        self.linearAcceleration = dict(x=0, y=0)
+        self.rotationalVelocity = 0
+        self.rotationalAcceleration = 0
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
