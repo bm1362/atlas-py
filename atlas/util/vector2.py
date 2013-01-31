cd78b08b979a1a341464bf6bf3c4f8aa29d5d2b3
Kyle Jones <kyle@jonesetc.com>
2013-01-31 17:43:28 -0600

    get_angle

--- atlas/util/vector2.py
+++ atlas/util/vector2.py
@@ -9,3 +9,12 @@ def rotate_vector(vector, angle):
 
     result = dict(x = x * cosa - y * sina, y = x * sina + y * cosa)
     return result
+
+# find the angle between any two points
+def get_angle(point1, point2):
+    opp = math.fabs(point1['x'] - point2['x'])
+    adj = math.fabs(point1['y'] - point2['y'])
+    theta = math.atan2(opp, adj)
+
+    return theta
+
